65a575fc567883752decef5e9b9f27b184d824d0
timzo31 <69592997+timzo31@users.noreply.github.com>
2022-06-08 23:57:46 +0800

    JS Modal component.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,32 @@
 "use strict";
 
-/////////////SMOOTH SCROLLING///////////////
-const btnShowModal = document.querySelector('.btn--show-modal');
+/////////////MODAL///////////////
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+
+
+const openModal = (e) => {
+    e.preventDefalut();
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+
+const closeModal = () => {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
+    }
+});
 
 
 
