cd0ed07b5a86d615decf05a30446a9013404b3db
timzo31 <69592997+timzo31@users.noreply.github.com>
2022-06-10 00:24:00 +0800

    JS Menu fade animation, passing argument into handler.

--- js/script.js
+++ js/script.js
@@ -128,8 +128,26 @@ tabsContainer.addEventListener('click', function (e) {
    
 })
 
+//////////////////////////MENU FADE ANIMATION///////////////////////////
+const nav = document.querySelector('.nav');
 
+const handleHover = function (e) {
+    if (e.target.classList.contains('nav__link')) {
+        const link = e.target;
+        const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+        const logo = link.closest('.nav').querySelector('img');
 
-
+        siblings.forEach(el =>  {
+            if (el !== link) {
+                el.style.opacity = this;
+            }
+        });
+        logo.style.opacity = this;
+    }
+};
+// nav.addEventListener('mouseover', function (e) {handleHover(e, .5);});
+//Passing "argument" into handler
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
 
 
